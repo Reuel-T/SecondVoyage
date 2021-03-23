09ec62ff775b3a0688fca32714b7c5d035c713f9
Reueltyler Moonsamy <19012817@vcconnect.co.za>
2021-03-23 11:11:13 +0200

    Add project files.

---
+++ SecondVoyage/ListHolder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecondVoyage
+{
+    class ListHolder
+    {
+        private static ListHolder obj;
+
+
+    }
+}
---
+++ SecondVoyage/Vroom.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecondVoyage
+{
+    public class Vroom
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public double Price { get; set; }
+        public string Color { get; set; }
+
+        public Vroom(string make, string model, double price, string color)
+        {
+            Make = make;
+            Model = model;
+            Price = price;
+            Color = color;
+        }
+    }
+}
